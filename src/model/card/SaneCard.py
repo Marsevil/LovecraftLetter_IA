32d2b6897362305761e35005496166dcc78c47da
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2020-12-03 16:01:58 +0100

    feat(SaneCard) : Add SaneCard class
    - Add a constructor with the name, description and value of the card
    - Add effect function
    - Add hasInsane function wich return a boolean
    
    saneCard

---
+++ src/model/card/SaneCard.py
@@ -0,0 +1,18 @@
+class SaneCard:
+    """ SaneCard class (abstract) is defined by :
+        - SaneCard(_name : string, _description : string, _value : int)
+        - effect(sanity : Sanity)
+        - hasInsane() """
+
+
+    def __init__(self, _name, _description, _value):
+        self.name = _name
+        self.description = _description
+        self.value = _value
+
+
+    def effect(sanity): pass
+
+    def hasInsane():
+        return False
+        
