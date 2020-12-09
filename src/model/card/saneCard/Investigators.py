4de37a352fdbe095de7dc97247cbd3b77259afd4
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2020-12-09 16:18:10 +0100

    test pour import

--- src/model/card/saneCard/Investigators.py
+++ src/model/card/saneCard/Investigators.py
@@ -1,7 +1,15 @@
-from SaneCard import SaneCard
-
+from ..SaneCard import SaneCard
 class Investigators (SaneCard):
 
-    @abstractmethod
     def __init__(self):
-        super().__init__(_name, _description, _value)
+        super().__init__("Investigators", "When you discard Investigators" +
+                            "during your turn, choose another player and name" +
+                            "a number (other than 1). If that player has that" +
+                            "number in their hand, they are knocked out of" +
+                            "the round. If all other players still in the" +
+                            "round cannot be chosen (e.g due to Elder Sign or" +
+                            "Liber Ivonis), this card is discarded without" +
+                            "effect." , 1)
+
+    def effect(self) :
+        print('Not implemented yet')
