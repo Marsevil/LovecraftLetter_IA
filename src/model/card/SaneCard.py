87b4fa1ca00a6abba59ab0f1b0330126587f1660
David <Pendorex@gmail.com>
2020-12-09 14:23:34 +0100

    Edit : editing of SaneCard import

--- src/model/card/SaneCard.py
+++ src/model/card/SaneCard.py
@@ -1,10 +1,14 @@
+from Card import Card
+from abc import ABC, abstractmethod
+
+
 class SaneCard (Card):
     """ SaneCard class (abstract) is defined by :
         - SaneCard(_name : string, _description : string, _value : int)
         - effect(sanity : Sanity)
         - hasInsane() """
 
-
+    @abstractmethod
     def __init__(self, _name, _description, _value):
         super().__init__(_name, _description, _value)
 
