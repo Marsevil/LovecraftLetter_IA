33733b5556ae7ce2f24834467cee8b458ec4c590
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2020-12-10 14:23:22 +0100

    refactor(CatsOfUlthar): Card Description looks better
    - Change the card description to fit into 80 characters per line
    
    specifiedCard

--- src/model/card/saneCard/CatsOfUlthar.py
+++ src/model/card/saneCard/CatsOfUlthar.py
@@ -1,12 +1,16 @@
 from ..SaneCard import SaneCard
 
 class CatsOfUlthar(SaneCard):
-    
+
     def __init__(self):
         super(CatsOfUlthar,self).__init__("CatsOfUlthar",
-                       "When you discard Cats of Ulthar during your turn, choose another player and look at their hand. Do not reveal it to any other players. If all other players still in the round cannot be chosen (e.g due to Elder Sign or Liber Ivonis), this card is discarded without effect.)",
-                       2)
-    
+                       "When you discard Cats of Ulthar during your turn," +
+                       " choose another player and look at their hand. Do" +
+                       " not reveal it to any other players. If all other" +
+                       " players still in the round cannot be chosen (e.g" +
+                       " due to Elder Sign or Liber Ivonis), this card is" +
+                       " discarded without effect.)", 2)
+
     @property
     def sanity(self):
         return self._sanity
@@ -19,9 +23,9 @@ class CatsOfUlthar(SaneCard):
 
     def getSanity(self):
         return self._sanity
-    
+
     def effect(self,gameManager):
         pass
-    
+
 chat = CatsOfUlthar()
 print(chat.value)
