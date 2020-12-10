9fdf07e5030fd901d71e95df1af85a1ef34e2657
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2020-12-10 16:46:28 +0100

    feat(saneCards): Add Description
    - Add sane cards descriptions
    
    specifiedCard

--- src/model/card/saneCard/Investigators.py
+++ src/model/card/saneCard/Investigators.py
@@ -4,13 +4,11 @@ class Investigators (SaneCard):
 
     def __init__(self):
         super().__init__("Investigators", "When you discard Investigators" +
-                            "during your turn, choose another player and name" +
-                            "a number (other than 1). If that player has that" +
-                            "number in their hand, they are knocked out of" +
-                            "the round. If all other players still in the" +
-                            "round cannot be chosen (e.g due to Elder Sign or" +
-                            "Liber Ivonis), this card is discarded without" +
-                            "effect." , 1)
+        " during your turn, choose another player and name a number (other" +
+        " than 1). If that player has that number in their hand, they are" +
+        " knocked out of the round. If all other players still in the round" +
+        " cannot be chosen (e.g due to Elder Sign or Liber Ivonis), this card" +
+        " is discarded without effect." , 1)
 
     @property
     def sanity(self):
