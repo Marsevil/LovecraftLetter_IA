11d7f532399a95cb3bbd09a5ea4b54afc7a7c552
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2020-12-11 19:29:15 +0100

    feat(TheSilverKey): Add card effect
    - Adding the effect but its still uncomplete
    
    specifiedCard

--- src/model/card/saneCard/TheSilverKey.py
+++ src/model/card/saneCard/TheSilverKey.py
@@ -27,3 +27,16 @@ class TheSilverKey (SaneCard):
 
     def effect(self, gameManager):
         pass
+
+
+    def whenDraw(self, gameManager):
+        # Get player hand
+        player = gameManager.getCurrentPlayer()
+        playerHand = player.getHand()
+
+        # Check if the card in hand before picking Silver Key has a value > 4
+        if(playerHand[0].getValue() > 4):
+            # Insérer fonction qui oblige le joueur à discard playerHand[1]
+
+        # Sinon il peut jouer la carte qu'il veut donc je crois qu'il n'y a
+        # rien à rajouter ici
