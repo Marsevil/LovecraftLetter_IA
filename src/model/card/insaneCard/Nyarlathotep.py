5bb0f54e24d953be21d6ba07a540d8409eb5fbc1
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2020-12-14 16:27:23 +0100

    feat(Nyarlathotep): Adding card
    - Add Nyarlathotep card
    
    specifiedCard

--- src/model/card/insaneCard/Nyarlathotep.py
+++ src/model/card/insaneCard/Nyarlathotep.py
@@ -1,1 +1,53 @@
-from InsaneCard import InsaneCard
+from ..InsaneCard import InsaneCard
+from ..Sanity import Sanity
+
+class Nyarlathotep(InsaneCard):
+
+    def __init__(self):
+        super(Nyarlathotep,self).__init__("Nyarlathotep", "Sane :" +
+        " When you discard Randolph Carter" +
+        " during your turn, choose another player and trade your hand with" +
+        " them. You cannot trade with a player who has been knocked out of" +
+        " the round.\n Insane : When you discard Nyarlathotep during your" +
+        " turn, collect the hands of all the other players still in the" +
+        " round. You may look at them. Then redistribute one card of your" +
+        " choice to each player. This effect does not put their cards in your" +
+        " hand, thus you cannot give the card you have in hand away.", 6)
+
+
+    @property
+    def sanity(self):
+        return self._sanity
+
+
+    @sanity.setter
+    def sanity(self, newvalue):
+        self._sanity = newvalue
+
+
+    def getSanity(self):
+        return self._sanity
+
+
+    def effect(self, gameManager):
+
+        if self.sanity == Sanity.SANE:
+            #Demande à viser un autre joueur
+            chosenOne = gameManager.chooseTargetPlayer(1, False)
+
+            #Vérifie qu'un joueur non immunisé a pu être choisi
+            if(len(chosenOne) != 0):
+
+                #Échange la main avec le joueur
+                playerHand = gameManager.getCurrentPlayer().getHand()
+                targetHand = chosenOne.getHand()
+
+                temp = playerHand[0]
+                playerHand[0] = targetHand[0]
+                targetHand[0] = temp[0]
+
+        if self.sanity == Sanity.INSANE:
+            # Call the function to show all the players card and to redistribute
+            gameManager.redistribute()
+
+    
