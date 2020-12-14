42be8c3d13944fb0dde80018709fef75e44e59af
David <Pendorex@gmail.com>
2020-12-14 11:23:31 +0100

    Add : MiGo && MiGoBraincase implementation

--- src/model/card/insaneCard/MiGoBraincase.py
+++ src/model/card/insaneCard/MiGoBraincase.py
@@ -1,1 +1,30 @@
-from InsaneCard import InsaneCard
+from ..InsaneCard import InsaneCard
+from ..Sanity import Sanity
+
+class MiGoBraincase(InsaneCard):
+    
+    def __init__(self):
+        super(MiGoBraincase,self).__init__("Mi-Go Braincase", "Sane : When you discard" 
+             +"this card during your turn, you lose the round."
+             +"\n Insane : When you discard this card during your turn, you lose the round",0)
+        
+        
+        
+    @property
+    def sanity(self):
+        return self._sanity
+
+
+    @sanity.setter
+    def sanity(self, newvalue):
+        self._sanity = newvalue
+
+
+    def getSanity(self):
+        return self._sanity
+
+
+    def effect(self, gameManager):
+        
+        if self.sanity == Sanity.SANE or self.sanity == Sanity.INSANE:
+            gameManager.getCurrentPlayer().setKnockedOut(True)
