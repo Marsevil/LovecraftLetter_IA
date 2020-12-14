e5e383d364aae5dc7af7759ef8b57820f3412424
David <Pendorex@gmail.com>
2020-12-14 09:53:47 +0100

    Edit : Investigators effect, Add : Implementations of DeepOnes

--- src/model/card/saneCard/Investigators.py
+++ src/model/card/saneCard/Investigators.py
@@ -28,14 +28,14 @@ class Investigators (SaneCard):
 
         #Demande à viser un autre joueur
         chosenOne = gameManager.chooseTargetPlayer(1, False)
-
+    
         #Vérifie qu'un joueur non immunisé a pu être choisi
         if(len(chosenOne) != 0):
-
+    
             #Demande à choisir un nombre supérieur à 1
-            chosenCard = gameManager.chooseNumber()
-
+            chosenCardNumber = gameManager.chooseNumber()
+    
             #Éjecte le joueur désigné s'il possède la carte choisie et qu'il peut être éjecté
             targetHand = chosenOne.getHand()
-            if ((targetHand[0] == chosenCard) and chosenOne.isKnockableOut()):
+            if ((targetHand[0].value == chosenCardNumber) and chosenOne.isKnockableOut()):
                 chosenOne.setKnockedOut(True)
